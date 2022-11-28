7d3a2588e6c3e6f9a109d808b6b6eea193bb7029
dfangulo <58793508+dfangulo@users.noreply.github.com>
2022-11-28 11:27:38 -0700

    Se agrego los css de bootstrap, ademas se cambio de sistema operativo a windows

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ require_once "class/xml.php";
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>XML PBR</title>
-        <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
     </head>
     <body>
         <nav class="navbar navbar-dark bg-dark">
